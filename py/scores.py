a03dbc5a3ad25e132a9e79a580e6f863ec2bfc6a
Phil Chodrow <pchodrow@gmail.com>
2020-04-28 14:00:20 -0400

    various experiments

--- py/scores.py
+++ py/scores.py
@@ -6,15 +6,22 @@ from SpringRank import SpringRank
 SpringRank_score = SpringRank
 
 @jit(nopython=True)
-def homebrew_SpringRank_score(A, alpha = 10**(-8)):
+def homebrew_SpringRank_score(A, alpha = 10**(-8), bias = None):
     Di = np.diag(A.sum(axis = 0))
     Do = np.diag(A.sum(axis = 1))
-    L = Di + Do - A - A.T + alpha*np.eye(A.shape[0])
+    
+    if bias is not None:
+#         bias = bias / bias.sum() * A.shape[0]
+        regularizer = alpha * np.diag(bias)
+    else:
+        regularizer = alpha * np.eye(A.shape[0])
+    
+    L = Di + Do - A - A.T + alpha*regularizer
     return(np.linalg.inv(L).dot(Do - Di).dot(np.ones(A.shape[0])))
 
 @jit(nopython=True)
-def powered_degree_score(A,p):
-	return(A.sum(axis = 0)**p)
+def powered_degree_score(A,p = 1):
+    return(A.sum(axis = 0)**p)
 
 @jit(nopython=True)
 def fiedler_vector_score(A):
@@ -28,7 +35,7 @@ def fiedler_vector_score(A):
     v = eig[1][:,1]    
     v = v / np.sqrt((v**2).sum())
 
-    return(v)	
+    return(v)   
 
 @jit(nopython=True)
 def katz_score(A, alpha = .001):
@@ -54,3 +61,24 @@ def PageRank_score(A, n_iter = 30, alpha = 0.15):
         v = -v
     return(v/v.sum())
 
+def eigenvector_score(A, n_iter = 30):
+    n = A.shape[0]
+    v = np.random.rand(n)
+    for i in range(n_iter):
+        v = A.dot(v)
+        v = v / v.sum()
+    return(v)
+
+# @jit(nopython=True)
+def RW_score(A, p = .75, alpha = 0):
+    wins = A.T
+    losses = A
+    D_wins = np.diag(wins.sum(axis = 1))
+    D_losses = np.diag(losses.sum(axis = 1))
+    M = p*(wins - D_losses) + (1-p)*(losses - D_wins) + alpha*np.eye(A.shape[0])
+    eigs = np.linalg.eig(M)
+    which_eig = np.argmin(np.abs(np.linalg.eig(M)[0]))
+    val = eigs[0][which_eig]
+    v = eigs[1][:,which_eig]
+    v = np.abs(v)
+    return(v)
